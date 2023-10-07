70e9f4492378580299be76c8198550cbb6a9ce7d
takokke <138221752+takokke@users.noreply.github.com>
2023-10-07 07:47:59 +0000

    viewを表示するためのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  
+  resources :books, only: [:edit, :index, :show]
+  resources :users, only: [:index, :show, :edit]
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
