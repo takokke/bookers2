60a3bcf10181ab478647812259e7296f9256e0c9
takokke <138221752+takokke@users.noreply.github.com>
2023-10-12 00:22:01 +0000

    booksのdestroy完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,11 @@ class BooksController < ApplicationController
     @newBook = Book.new()
   end
 
-
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
   private
 
