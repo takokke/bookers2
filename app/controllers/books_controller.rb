836c6897d2dab30070c6d68c6edc84e0d22b695f
takokke <138221752+takokke@users.noreply.github.com>
2023-10-13 14:04:21 +0000

    バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(id: @book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(id: @book.id)
+    else
+      @books = Book.all
+      render "index"
+    end
   end
 
   def edit
@@ -18,9 +22,12 @@ class BooksController < ApplicationController
 
   def update
     @book = current_user.books.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(id: @book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(id: @book.id)
+    else
+      render "edit"
+    end
   end
 
   def show
