eb8a6096c726790805b7b0b75f8fa2312dc1cae5
takokke <138221752+takokke@users.noreply.github.com>
2023-10-13 13:12:55 +0000

    flashメッセージ実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(id: @book.id)
   end
 
@@ -18,6 +19,7 @@ class BooksController < ApplicationController
   def update
     @book = current_user.books.find(params[:id])
     @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(id: @book.id)
   end
 
